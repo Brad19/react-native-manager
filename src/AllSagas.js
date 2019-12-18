5ab5956509d31e336351e94cb88cf59635154764
Brad19 <btbrad1979@gmail.com>
2019-12-19 00:09:04 +1100

    Remove console log message AllSagas.js

--- src/AllSagas.js
+++ src/AllSagas.js
@@ -2,7 +2,6 @@ import { all } from 'redux-saga/effects';
 import fetchUserSaga from './components/sagas';
 
 export default function *AllSagas() {
-    console.log('calling AllSagas');
     try {
         yield fetchUserSaga;
     } catch(error) {
