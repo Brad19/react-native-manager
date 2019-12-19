89e6df275500c482d049766ed1b8355e7caf75c4
Barani Thiruvengadam <Barani.Thiruvengadam@ofx.com>
2019-12-20 07:29:43 +1100

    react-native-manager - Add API Key - Modify Saga function call

--- src/AllSagas.js
+++ src/AllSagas.js
@@ -3,7 +3,7 @@ import fetchUserSaga from './components/Login/saga/fetchUserSaga';
 
 export default function *AllSagas() {
     try {
-        yield all([fetchUserSaga]);
+        yield all([fetchUserSaga()]);
     } catch(error) {
         console.log('error - allsagas :', error);
     }
